03d75763bb64f78ce0f7d16cc57414b3ef47f004
lycho33 <cho.lyds@gmail.com>
2021-08-23 17:17:21 -0400

    commit tree 9f4c89855ad2419445693d34e49813e2ec9c63dd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYouMessage = []
+    for (let i = 0; i < name.length; i++) {
+        thankYouMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`) // push is what you needed
+    }
+    return thankYouMessage
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number); //shows the first number(10)
+        number -= 1; //iterates down
+    }
+    return console.log(number); //prints all the numbers
+}
